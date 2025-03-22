5b8a1256ec9be870d0ffe43a3e66b30f6eb035e0
Richard Madjos <drahcirmadjos16@gmail.com>
2025-03-22 18:34:13 +0800

    flowbite & css

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const app = express()
 app.set("view engine", "ejs");
 app.set("views", "./views");
 const port = 3000;
+const axios = require("axios");
 
 const userRouter = require("./routers/users");
 app.use("/users", userRouter);
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-  res.render('index', );
+    const response = await
+axios.get('https://jsonplaceholder.typicode.com/posts');
+  res.render('index', { posts: response.data });
+} catch (error) {
+  res.status(500).send('Error fetching data');
+}
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
